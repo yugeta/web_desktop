9750a864e010d2274561cffd44a6d79ec3b7d357
Yugeta.Koji <geta1972@gmail.com>
2024-10-15 08:16:00 +0900

    #19

--- js/lib/bootstrap.js
+++ js/lib/bootstrap.js
@@ -1,6 +1,9 @@
 export class Bootstrap{
   static get elm_main(){
-    return document.querySelector("main.desktop")
+    return document.querySelector("#desktop main.desktop")
+  }
+  static get elm_header(){
+    return document.querySelector("#desktop header")
   }
   static get window_rect(){
     return Bootstrap.elm_main.getBoundingClientRect()
